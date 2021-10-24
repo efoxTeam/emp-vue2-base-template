7c129f448ced8b97a0d7b585cd6955f19bdde31f
flyyuan <740004544@qq.com>
2021-10-24 10:58:33 +0800

    feat: 更新模版到 1.9

--- emp-config.js
+++ emp-config.js
@@ -1,45 +1,35 @@
-const withVue2 = require('@efox/emp-vue2')
-module.exports = withVue2(({config}) => {
-  const projectName = 'vue2Base'
-  const port = 8009
-  config.output.publicPath(`http://localhost:${port}/`)
-  config.devServer.port(port)
-  config.plugin('mf').tap(args => {
-    args[0] = {
-      ...args[0],
-      ...{
-        /**
-         * name: 对外暴露项目名,
-         */
-        name: projectName,
-        filename: 'emp.js',
-        /**
-         * exposes 对外暴露的模块
-         *  exposes: {
-         *    '模块对外暴露时的相对路径':'当前项目相对路径'
-         *  }
-         */
-        exposes: {
-          './Content': './src/components/Content',
+/**
+ * @type {import('@efox/emp-cli').EMPConfig}
+ */
+const mf = require('./emp_config/mf')
+const port = 8009
+module.exports = {
+  framework: [require('@efox/emp-vue2')],
+  webpackChain(config) {
+    config.plugin('html').tap(args => {
+      args[0] = {
+        ...args[0],
+        ...{
+          title: 'EMP Vue2 Base',
         },
-        // shared: ['vue/dist/vue.esm.js'],
+      }
+      return args
+    })
+
+    // 配置 svg loader
+    const svgRule = config.module.rule('svg')
+    svgRule.uses.clear()
+    svgRule.use('vue-svg-loader').loader('vue-svg-loader')
+  },
+  webpack() {
+    return {
+      devServer: {
+        port: port,
       },
     }
-    return args
-  })
-
-  config.plugin('html').tap(args => {
-    args[0] = {
-      ...args[0],
-      ...{
-        title: 'EMP Vue2 Base',
-      },
-    }
-    return args
-  })
-
-  // 配置 svg loader
-  const svgRule = config.module.rule('svg')
-  svgRule.uses.clear()
-  svgRule.use('vue-svg-loader').loader('vue-svg-loader')
-})
+  },
+  async moduleFederation({empEnv}) {
+    console.log('empEnv', empEnv)
+    return mf(empEnv)
+  },
+}
